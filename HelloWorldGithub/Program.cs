a5e8ebc46444b02c6e794ece1ddf707cda54ca60
ThatRandomGamer1337 <113189409+ThatRandomGamer1337@users.noreply.github.com>
2022-09-12 11:06:04 -0300

    first commit

---
+++ HelloWorldGithub/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldGithub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("- - - - - - - - - - - - - - - -");
+            Console.WriteLine($"Hello World!");
+            Console.WriteLine("- - - - - - - - - - - - - - - -");
+            Console.Beep();
+            Console.WriteLine(" ");
+            Console.WriteLine(" ");
+            Console.ReadKey(true);
+        }
+    }
+}
